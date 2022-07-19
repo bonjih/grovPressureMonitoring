c6b5abeb20aaae5280919b124f38bbf6b44f778b
bonjih <bojih@gmail.com>
2022-07-19 15:43:08 +1000

    formatting

--- db_manager.py
+++ db_manager.py
@@ -35,23 +35,4 @@ class SQL:
         df.to_sql('pulse_shield_locator', con=self.engine, if_exists='append', index=False)
 
 
-# gets last ventilation velocity value
-def pi_query_vent():
-    conn = pyodbc.connect(
-        "Driver=PI SQL Client; AF Server=AISGROOSI01; AF Database='Grosvenor MAC'; Integrated Security=SSPI;",
-        autocommit=True)
-    select_statement = f'''
-                SELECT a.TimeStamp
-                ,a.Value_Double
-                FROM Element.Element e
-                inner join Element.Attribute ea ON ea.ElementID = e.ID
-                inner join Element.Archive a ON a.AttributeID = ea.ID
-                inner join Element.ElementHierarchy eh ON eh.ElementID = ea.ElementID
-                WHERE e.Name in ('Pressure Monitoring')
-                and ea.name in ('Ventillation Velocity')
-                and a.TimeStamp > '{current_date}'
-                '''
-    df = pd.read_sql(select_statement, conn)
-    df = df.iloc[-1:]
-    df = df['Value_Double']
-    return df.to_string()[7:]
+
